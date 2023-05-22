c7b13f2470f89efbeae1f58a91e579f22eb62e96
jhoncabrera <jhoncabrera2050@gmail.com>
2023-05-21 21:31:34 -0500

    BackEnd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 const db = require('./conection');
+app.use(cors());
 app.use(express.json());
 app.use(require('../src/routes/index'))
 
